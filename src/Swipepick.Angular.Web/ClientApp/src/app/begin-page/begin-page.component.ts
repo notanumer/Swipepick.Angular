27c91844455f7cdedb7a9972b274265e8083af43
Михаил <gmike103g@gmail.com>
2023-05-04 21:35:51 +0500

    the login page and the main page have been moved

---
+++ src/Swipepick.Angular.Web/ClientApp/src/app/begin-page/begin-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-begin-page',
+  templateUrl: './begin-page.component.html',
+  styleUrls: ['./begin-page.component.css']
+})
+export class BeginPageComponent {
+
+}
---
+++ src/Swipepick.Angular.Web/ClientApp/src/app/shared/services/auth-api.service.ts
@@ -0,0 +1,46 @@
+import {Inject, Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {catchError, Observable, tap} from "rxjs";
+import {UserLogin, UserLoginResponse, UserRegister} from "../interfaces/auth-interfaces";
+
+@Injectable()
+export class AuthApiService {
+
+  constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) {}
+
+  get token(): string | null {
+    return localStorage.getItem('token')
+  }
+
+  login(user: UserLogin): Observable<any> {
+    return this.http.post(this.baseUrl + 'api/auth/login', user)
+
+
+  }
+
+  autoLogin(token: string | null): Observable<any> | void{
+    if (token) {
+      return this.http.post(this.baseUrl + 'api/auth/login', token)
+    }
+  }
+
+  register(user: UserRegister): Observable<any> {
+    return this.http.post(this.baseUrl + 'api/auth/register', user)
+  }
+
+  logout() {
+    this.setToken(null)
+  }
+
+  isAuthenticated(): boolean {
+    return !!this.token
+  }
+
+  private setToken(response: UserLoginResponse | null) {
+    if (response) {
+      localStorage.setItem('token', response.token)
+    } else {
+      localStorage.clear()
+    }
+  }
+}
