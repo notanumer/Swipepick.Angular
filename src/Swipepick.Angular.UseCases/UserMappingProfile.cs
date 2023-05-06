7e01980444db7e3789e46c7d39ba978c41fdf7b7
notanumer <kolchin-03@inbox.ru>
2023-05-06 12:42:41 +0500

    Add test for AutoMapper

--- src/Swipepick.Angular.UseCases/UserMappingProfile.cs
+++ src/Swipepick.Angular.UseCases/UserMappingProfile.cs
@@ -8,6 +8,11 @@ public class UserMappingProfile : Profile
 {
     public UserMappingProfile()
     {
-        CreateMap<AddUserCommand, User>();
+        CreateMap<AddUserCommand, User>()
+            .ForMember(u => u.PasswordHash, dest => dest.Ignore())
+            .ForMember(u => u.PasswordSalt, dest => dest.Ignore())
+            .ForMember(u => u.Tests, dest => dest.Ignore())
+            .ForMember(u => u.Students, dest => dest.Ignore())
+            .ForMember(u => u.Id, dest => dest.Ignore());
     }
 }
---
+++ src/Swipepick.Angular.Web.Tests/AutoMapperTests.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using Swipepick.Angular.Web.Infrastructure.DependencyInjection;
+using Xunit;
+
+namespace Swipepick.Angular.Web.Tests;
+
+public class AutoMapperTests
+{
+    [Fact]
+    public void AutoMapper_Configuration_Valid()
+    {
+        // Arrange
+        var sc = new ServiceCollection();
+        AutoMapperModule.Register(sc);
+
+        // Act
+        var serviceProvider = sc.BuildServiceProvider();
+        var mapper = serviceProvider.GetRequiredService<IMapper>();
+
+        // Assert
+        mapper.ConfigurationProvider.AssertConfigurationIsValid();
+    }
+}
