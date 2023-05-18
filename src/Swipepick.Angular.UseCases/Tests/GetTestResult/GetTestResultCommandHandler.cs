dfc295874b4032203a6a9228bdd8f253d03078b9
notanumer <kolchin-03@inbox.ru>
2023-05-18 23:20:12 +0500

    Rework calculating result logic

--- src/Swipepick.Angular.UseCases/Tests/GetTestResult/GetTestResultCommandHandler.cs
+++ src/Swipepick.Angular.UseCases/Tests/GetTestResult/GetTestResultCommandHandler.cs
@@ -15,15 +15,23 @@ public class GetTestResultCommandHandler : IRequestHandler<GetTestResultCommand,
 
     public async Task<int> Handle(GetTestResultCommand request, CancellationToken cancellationToken)
     {
-        var currectAnsw = request.StudentAnswer.SelectedAnswers.GroupBy(x => x.QuestionId);
-        var test = await appDbContext.Tests.FirstAsync(t => t.UniqueCode == request.TestCode, cancellationToken);
-        var questions = test.Questions.Select(x => x).ToList();
+        var currentAnsws = request.StudentAnswer.SelectedAnswers.GroupBy(x => x.QuestionId);
+
+        var test = await appDbContext.Tests
+            .Include(t => t.Questions)
+            .ThenInclude(q => q.Answers)
+            .ThenInclude(a => a.AnswerVariants)
+            .FirstAsync(t => t.UniqueCode == request.TestCode, cancellationToken);
+
         var count = 0;
-        var correctAnsw = questions.Select(x => x.Answers.First()).GroupBy(x => x.QuestionId);
-        foreach (var t in currectAnsw)
+        var correctAnsw = test.Questions
+            .Select(x => x.Answers.First())
+            .GroupBy(x => x.QuestionId);
+
+        foreach (var t in currentAnsws)
         {
             var group = correctAnsw.Single(g => g.Key == t.Key);
-            var y = currectAnsw.Single(g => g.Key == t.Key);
+            var y = currentAnsws.Single(g => g.Key == t.Key);
             var currentAns = y.Select(x => x).First();
             var tight = group.Select(x => x).First();
             if (tight.CorrectAnswer == currentAns.AnswerCode)
